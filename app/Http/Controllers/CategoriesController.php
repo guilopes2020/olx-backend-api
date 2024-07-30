63c19875392b81c8ef39fc8d31cf9513fb29e0c2
guilopes2020 <guilopesdev@gmail.com>
2024-07-30 15:23:36 -0300

    feat: :sparkles: creating-controllers-and-routes-and-seeders

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class CategoriesController extends Controller
+{
+    public function index(Request $request): JsonResponse
+    {
+        $categories = Category::all();
+        return response()->json(array('categories' => $categories));
+    }
+}
---
+++ app/Http/Requests/CreateUserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class CreateUserRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'     => 'required|string|min:3',
+            'email'    => 'required|email|string|unique:users',
+            'password' => 'required|string|min:6',
+            'state_id' => 'required|exists:states,id'
+        ];
+    }
+
+    public function failedValidation(Validator $validator): void
+    {
+        throw new HttpResponseException(response()->json([
+            'errors' => $validator->errors(),
+            'status' => 'error',
+        ]));
+    }
+}
